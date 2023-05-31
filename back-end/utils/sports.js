821f1b1d3971261967fe09647a83446be7088458
Tayyyyylor <bryan.h.pro@gmail.com>
2023-05-31 14:41:07 +0200

    sports routes sql

--- back-end/utils/sports.js
+++ back-end/utils/sports.js
@@ -9,7 +9,26 @@ export const pool = mysql
 })
 .promise();
 
-export const getSportName = async () => {
+export const getSportNameSQL = async () => {
     const [rows] = await pool.query('SELECT * FROM sport');
     return rows;
 }
+
+export const deleteSportSQL = async (id) =>
+{
+  await pool.query("DELETE FROM sport WHERE id = ? ", [id]);
+};
+
+export const createSportSQL = async (nom) => {
+    await pool.query("INSERT INTO sport (nom) VALUES (?)", [nom]);
+  };
+
+  export const modifySportSQL = async (nom,id) =>
+{
+    await pool.query("UPDATE sport SET nom = ? WHERE id = ?",[nom,id])
+}
+
+export const getSportSQL = async (id) => {
+    const [rows] = await pool.query("SELECT * FROM sport where id = ?", [id]);
+    return rows[0];
+  };
