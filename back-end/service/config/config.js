a6679cf2445e63e9d86b96de810158fa7a778a59
lies20 <hocini.lies@gmail.com>
2023-05-31 09:56:18 +0200

    Ajout du service

---
+++ back-end/service/config/config.js
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+	db: {
+		host: process.env.DB_HOST,
+		name: process.env.DB_NAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+	},
+};
