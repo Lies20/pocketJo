b43f015d7d638d51dcd8124f6bd263a29d545528
ahamadaKhaled <khaahamada@publicisgroupe.net>
2023-06-01 10:11:51 +0200

    Revert "fix-athlete"
    This reverts commit 1c4bd31790d8d1624fe6bb92d6665524e35eae8e.

--- back-end/utils/athlete.js
+++ back-end/utils/athlete.js
@@ -9,19 +9,19 @@ export const getAthleteNameSQL = async () => {
 
 export const deleteAthleteSQL = async (id) =>
 {
-  await pool.query("DELETE FROM athlete WHERE id = ? ", [id]);
+  await pool.query("DELETE FROM sport WHERE id = ? ", [id]);
 };
 
 export const createAthleteSQL = async (nom) => {
-    await pool.query("INSERT INTO athlete (nom) VALUES (?)", [nom]);
+    await pool.query("INSERT INTO sport (nom) VALUES (?)", [nom]);
   };
 
   export const updateAthleteSQL = async (nom,id) =>
 {
-    await pool.query("UPDATE athlete SET nom = ? WHERE id = ?",[nom,id])
+    await pool.query("UPDATE sport SET nom = ? WHERE id = ?",[nom,id])
 };
 
 export const getAthleteSQL = async (id) => {
-    const [rows] = await pool.query("SELECT * FROM athlete where id = ?", [id]);
+    const [rows] = await pool.query("SELECT * FROM sport where id = ?", [id]);
     return rows[0];
   };
