635c90ffe667bc4ae5c02dc3a70638e4ca2b072b
Tayyyyylor <bryan.h.pro@gmail.com>
2023-06-01 10:45:15 +0200

    fix sur nav

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Athletes from './pages/Athletes'
 import Homepage from './pages/Homepage'
 import { Routes, Route } from "react-router-dom"
+import Sports from './pages/Sports'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Homepage/>}/>
+      <Route path='/sports' element={<Sports/>}/>
+      <Route path='/athletes' element={<Athletes/>}/>
     </Routes>
  
   )
