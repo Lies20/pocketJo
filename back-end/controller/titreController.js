f79a79d25171d8e161485952ff96a776b1f53bec
Tayyyyylor <bryan.h.pro@gmail.com>
2023-06-02 14:22:05 +0200

    correctif back

--- back-end/controller/titreController.js
+++ back-end/controller/titreController.js
@@ -2,14 +2,14 @@ import { getTitreSQL, createTitreSQL } from "../utils/titre.js";
 
 export const getTitre = async (req, res) => {
     const titres = await getTitreSQL();
-    res.json(athletes);
+    res.json(titres);
 };
 
 export const createTitre = async (req, res) => {
     const {athlete_id, epreuve_id, medaille_id} = req.body;
     try {
 		await createTitreSQL(athlete_id, epreuve_id, medaille_id);
-		res.json({ message: `GO FUCK YOURSELF!!` });
+		res.json({ message: `GO FUCK YOURSELF!! <3` });
 	} catch (error) {
 		console.log("error", error);
 	}
--- back-end/utils/pool.js
+++ back-end/utils/pool.js
@@ -1,12 +1,14 @@
 import mysql from "mysql2";
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 export const pool = mysql 
 .createPool({
-    host: "localhost",
-	database: "dataJO",
-    password: "",
-    user:"root",
+    host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 
 })
 .promise();
